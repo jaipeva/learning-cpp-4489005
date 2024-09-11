3f4d904b760907f1c9cab651427ddacde3584b93
JP <jaipeva@hotmail.com>
2024-09-11 12:13:52 +0000

    CodeDemo.cpp

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,14 @@
 // Learning C++ 
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+#include <iostream>
+
+int main(){
+    std::string str;
+    
+    std::cout << "Hello To World" <<std::endl;
+    std::cout << "Please enter a name :" << std::flush;
+    std::cin >> str; 
+    std::cout << "The name is = " << str << std::endl << std::endl;
+    return(0);
+}
